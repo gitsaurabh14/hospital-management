5eb38d9b1167bde5b96f8c6df5bd657c1b26e443
Saurabh Singh <saurabhsingh7080875060@gmail.com>
2026-04-13 14:14:01 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ connectCloudinery();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(cors(
+  {
+    origin: "*",
+  }
+));
 
 //api end points
 app.use("/api/admin", adminRouter);
